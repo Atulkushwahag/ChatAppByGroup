08f4f13fda130dbc40b31f176b8753250751b38a
Atul Kushwaha <atulkushwahag@gmail.com>
2022-06-06 12:39:11 +0530

    Add project files.

---
+++ ChatApp/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+namespace ChatApp
+{
+    public class ChatHub : Hub
+    {
+       
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+        public async Task SendMessageToGroup(string groupName, string user, string message)
+    => await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+    }
+}
---
+++ ChatApp/wwwroot/js/Chat.js
@@ -0,0 +1,50 @@
+﻿var elList, counter;
+elList = document.getElementById('messagesList');
+counter = document.getElementById('counter');
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .build();
+
+//This method receive the message and Append to our list
+connection.on("ReceiveMessage", (user, message) => {
+    const msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const encodedMsg = user + " :: " + msg;
+    const li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+
+    listItems = elList.getElementsByTagName('li');
+    counter.innerHTML = listItems.length;
+
+     
+});
+
+connection.start().catch(err => console.error(err.toString()));
+
+//Send the message
+
+//document.getElementById("sendMessage").addEventListener("click", event => {
+//    const user = document.getElementById("userName").value;
+//    const message = document.getElementById("userMessage").value;
+//    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+//    event.preventDefault();
+//});
+
+
+document.getElementById("sendMessage").addEventListener("click", event => {
+    const groupName = document.getElementById("groupName").value;
+    const user = document.getElementById("userName").value;
+    const message = document.getElementById("userMessage").value;
+    counter.innerHTML = 0;
+    connection.invoke("SendMessageToGroup", groupName, user, message).catch(err => console.error(err.toString()));
+    event.preventDefault();
+   
+});
+
+
+document.getElementById("btnPrivateGroup").addEventListener("click", event => {
+    const groupName = document.getElementById("groupName").value;
+    connection.invoke("JoinGroup", groupName).catch(err => console.error(err.toString()));
+    event.preventDefault();
+});
+ 
